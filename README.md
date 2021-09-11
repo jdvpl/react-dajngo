5a15026c850f7a025a76ae837c21da9f63bcf427
jdvpl <juanda554242@gmail.com>
2021-09-10 23:31:36 -0500

    creando proyecto y colocando bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# react-dajngo
+# Reacr-Django este es el front 
+
+## Pasos 
+
+### Crear proyecto
+`
+npx create-react-app react-django
+`
+
---
+++ react-django/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App container">
+      <h3 className="d-flex justify-content-center m-">
+        React-Django Frontend
+      </h3>
+    </div>
+  );
+}
+
+export default App;
