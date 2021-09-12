b32aba6a6c12a188751266c04ac7b8ff4308b2b2
jdvpl <juanda554242@gmail.com>
2021-09-11 23:05:36 -0500

    TRAYENDO LOS DATOS DE LA API

---
+++ react-django/src/Variables.js
@@ -0,0 +1,4 @@
+export const variables={
+    API_URL:"https://django-api-jdvpl.herokuapp.com/",
+    PHOTO_URL:"https://django-api-jdvpl.herokuapp.com/images/",
+}
--- react-django/src/proyecto.js
+++ react-django/src/proyecto.js
@@ -1,10 +1,66 @@
 import React,{Component} from "react";
-
+import {variables} from './Variables.js';
 export class Proyecto extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state={
+            proyectos:[]
+        }
+    }
+
+    refreshList() {
+        fetch(variables.API_URL+'proyecto')
+        .then(response => response.json())
+        .then(data =>{
+            this.setState({proyectos:data})
+        });
+    }
+    componentDidMount() {
+        this.refreshList();
+    }
     render() {
+        const {proyectos} = this.state;
         return (
             <div>
                 <h3>Pagina de Proyecto</h3>
+
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Proyecto</th>
+                            <th>Descripcion</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {proyectos.map(pro=>
+                            <tr key={pro.ProyectotId}>
+                                <td>{pro.ProyectotId}</td>    
+                                <td>{pro.ProyectoNombre}</td>    
+                                <td>{pro.ProyectoDescripcion}</td>
+                                <td>
+                                    <button
+                                        type="button"
+                                        className="btn btn-dark mr-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                        <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                        </svg>
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-dark ml-1">
+                                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                    </svg>
+                                    </button>
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
             </div>
         )   
     }
